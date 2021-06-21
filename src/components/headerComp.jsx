ec9bd106ea6251246095d3ad97153e8db7cc4d54
Joshua White <jwhite@us.ci.org>
2021-06-21 16:45:41 -0400

    updated display

--- src/components/headerComp.jsx
+++ src/components/headerComp.jsx
@@ -2,7 +2,7 @@ import { Image } from 'react'
 import logo from '../media/logo.png'
 const HeaderComp = () => {
   return <header class="header">
-    <h1>Josh's Cat Cars</h1>
+    <h1>Auto Galaxy</h1>
     <div class="header-right">
       <img class="logo-img" src={logo} alt="logoooo" />
     </div>
