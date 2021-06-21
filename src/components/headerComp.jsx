6e34e6c338afaf8ef3d6d076907abbcb3d28c957
Joshua White <jwhite@us.ci.org>
2021-06-21 16:22:53 -0400

    Got header working decently

--- src/components/headerComp.jsx
+++ src/components/headerComp.jsx
@@ -1,13 +1,13 @@
 import { Image } from 'react'
 import logo from '../media/logo.png'
 const HeaderComp = () => {
-  return <div class="header">
-  <a href="#default" >Josh's Cat Cars</a>
-  <div class="header-right">
-    <img class="logo-img" src={logo} alt="logoooo" />
-  </div>
-</div>
-    
+  return <header class="header">
+    <h1>Josh's Cat Cars</h1>
+    <div class="header-right">
+      <img class="logo-img" src={logo} alt="logoooo" />
+    </div>
+  </header>
+
 }
 
 export default HeaderComp
